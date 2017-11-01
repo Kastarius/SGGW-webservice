5ab0fdc615959504756f4e55f32bc13c8fe78c1c
Kamil Mirkowski <mirkowski.kamil@gmail.com>
2017-11-01 21:42:00 +0100

    refactor, suport for local configuration

--- src/main/java/pl/sggw/support/webservice/config/SpringConfiguration.java
+++ src/main/java/pl/sggw/support/webservice/config/SpringConfiguration.java
@@ -1,7 +1,11 @@
 package pl.sggw.support.webservice.config;
 
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 /**
  * Created by Kamil on 2017-10-20.
@@ -16,6 +20,18 @@ import org.springframework.context.annotation.Configuration;
                 "pl.sggw.support.webservice.security"
         })
 public class SpringConfiguration {
+
+
+        @Bean
+        public static PropertyPlaceholderConfigurer properties() {
+                PropertyPlaceholderConfigurer ppc  = new PropertyPlaceholderConfigurer();
+                Resource[] resources = new ClassPathResource[]{new ClassPathResource("application.properties")
+                        ,new ClassPathResource("local.properties")};
+                ppc.setLocations(resources);
+                ppc.setIgnoreUnresolvablePlaceholders(true);
+                ppc.setIgnoreResourceNotFound(true);
+                return ppc;
+        }
 
 
 }
