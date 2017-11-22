318ad2564f25aa405fa375dca1fef0d25c20453b
Bartosz Rackowski <brac193@gmail.com>
2017-11-22 21:39:46 +0100

    edit task with change status method

--- src/main/java/pl/sggw/support/webservice/populator/TaskPopulator.java
+++ src/main/java/pl/sggw/support/webservice/populator/TaskPopulator.java
@@ -2,14 +2,8 @@ package pl.sggw.support.webservice.populator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import pl.sggw.support.webservice.dto.BasicUserData;
-import pl.sggw.support.webservice.dto.Category;
-import pl.sggw.support.webservice.dto.Priority;
-import pl.sggw.support.webservice.dto.Task;
-import pl.sggw.support.webservice.model.CategoryModel;
-import pl.sggw.support.webservice.model.PriorityModel;
-import pl.sggw.support.webservice.model.TaskModel;
-import pl.sggw.support.webservice.model.UserModel;
+import pl.sggw.support.webservice.dto.*;
+import pl.sggw.support.webservice.model.*;
 
 @Component
 public class TaskPopulator extends AbstractPopulator<TaskModel, Task> {
@@ -19,6 +13,9 @@ public class TaskPopulator extends AbstractPopulator<TaskModel, Task> {
 
     @Autowired
     CategoryPopulator categoryPopulator;
+
+    @Autowired
+    StatusPopulator statusPopulator;
 
     @Override
     public void populate(TaskModel source, Task target) {
@@ -45,6 +42,12 @@ public class TaskPopulator extends AbstractPopulator<TaskModel, Task> {
         userData.setFirstName(userModel.getFirstName());
         userData.setLastName(userModel.getLastName());
         target.setUserData(userData);
+
+        Status status = new Status();
+        StatusModel statusModel = source.getStatusModel();
+        status.setId(statusModel.getId());
+        status.setName(statusModel.getName());
+        target.setStatus(status);
     }
 
     @Override
@@ -55,6 +58,7 @@ public class TaskPopulator extends AbstractPopulator<TaskModel, Task> {
         target.setDescription(source.getDescription());
         target.setPriorityModel(convertToPriorityModel(source.getPriority()));
         target.setCategoryModel(convertToCategoryModel(source.getCategory()));
+        target.setStatusModel(convertToStatusModel(source.getStatus()));
 
         BasicUserData userData = source.getUserData();
         UserModel userModel = new UserModel();
@@ -74,5 +78,11 @@ public class TaskPopulator extends AbstractPopulator<TaskModel, Task> {
         CategoryModel categoryModel = new CategoryModel();
         categoryPopulator.reversePopulate(category, categoryModel);
         return categoryModel;
+    }
+
+    private StatusModel convertToStatusModel(Status status) {
+        StatusModel statusModel = new StatusModel();
+        statusPopulator.reversePopulate(status, statusModel);
+        return statusModel;
     }
 }
